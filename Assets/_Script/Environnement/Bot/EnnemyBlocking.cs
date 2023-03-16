99b196e02ec63f490ac8b85f42c7a03e3839c1d0
Zoreol <105099379+Zoreol@users.noreply.github.com>
2023-03-16 14:13:39 +0100

    leveldesign

--- Assets/_Script/Environnement/Bot/EnnemyBlocking.cs
+++ Assets/_Script/Environnement/Bot/EnnemyBlocking.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 
 public class EnnemyBlocking : EnnemiSmugManager
 {
-    private void Start()
+    float speedbase;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-         
+        
+        if (collision.CompareTag("Cak"))
+        {
+            speedbase = speed;
+            animator.Play("Slime_Hurt");
+            animator.SetBool("hurt", true);
+            speed = 0f;
+
+            StartCoroutine(InStone());
+        }
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerManager>().life--;
+        }
+    }
+
+    IEnumerator InStone()
+    {
+        
+        yield return new WaitForSeconds(5f);
+
+        gameObject.transform.position = waypoints[0].position;
+        animator.SetBool("hurt", false);
+        speed = speedbase;
     }
 }
